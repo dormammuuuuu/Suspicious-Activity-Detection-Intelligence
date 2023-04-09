117108f5bb8c60132d2038c21862b59ef259cf63
Jerico Victoria <78258443+dormammuuuuu@users.noreply.github.com>
2023-04-09 20:22:07 +0800

    Redesigned Sidebar & added Sidebar item Component

---
+++ sadi-react/src/components/SidebarLink.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+const SidebarLink = (props) => {
+    const link = props.link;
+    const label = props.label;
+    const children = props.children;
+
+    return (
+        <NavLink to={link}
+            className={({ isActive }) => clsx(
+            "flex items-center p-2 text-base font-normal rounded-lg hover:bg-gray-100",
+            isActive ? "text-blue-500 bg-violet-50" : " text-neutral-400"
+            )}>
+                {children}
+                <span className="ml-3">{label}</span>
+        </NavLink>
+    )
+}
+
+export default SidebarLink
