005c3f6a1c4126afbc442ff66766f1e34e3c0892
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2023-06-02 03:49:19 +0800

    Update the data of accordion list in  help menu

--- sadi-react/src/data/faq_data.js
+++ sadi-react/src/data/faq_data.js
@@ -1,43 +1,82 @@
 export const faq_data = [
    {
-      question: "What is YOLOv5?",
-      answer: "YOLOv5 is a state-of-the-art object detection algorithm that stands for You Only Look Once version 5. It is a deep learning model that can detect objects in images and videos with high accuracy and speed."
+      id: 1,
+      question: 'How to register a new user?',
+      answer: [
+         'Go to the Users menu and select the Add Icon in the upper right corner to add a new user. You will be directed to a new page where you can create an account after clicking the Add Icon.',
+         'You must finish the following 3 steps in order to register an account:',
+         '<strong>Step 1:</strong> Filling out your personal information is the first step. Your first and last names, username, email address, and other basic personal details are required in this phase. Password and mobile number.',
+         '<strong>Step 2:</strong> Registering your face is step two. To capture and register your face, you will be prompted to activate your webcam at this stage. In order to improve accuracy and security, you will be prompted to adjust your face in accordance with the required angle.',
+         '<strong>Step 3:</strong> Registration is finished. This will signify that you have successfully registered an account and been added to the list of users. It is the last step.'
+      ]
    },
    {
-      question: "What are the advantages of YOLOv5?",
-      answer: "YOLOv5 offers several advantages, including real-time object detection, high accuracy, and the ability to detect multiple objects within a single image. It is also lightweight and can run efficiently on various hardware platforms."
+      id: 2,
+      question: "How to change my password?",
+      answer: [
+         "Go to Account Settings and select Change Password Settings to modify your password. Your Old Password, New Password, and Confirm Password will then need to be entered. Next, save.",
+         'Once the change has been made successfully, you can log in to your account using your new password.'
+      ]
    },
    {
-      question: "How does YOLOv5 differ from previous versions?",
-      answer: "YOLOv5 is an improvement over previous versions of YOLO, with enhanced performance and accuracy. It introduces a new architecture, model scaling options, and advanced training techniques to achieve better object detection results."
+      id: 3,
+      question: "How to recover my account?",
+      answer: [
+         "Go to the login page and select the Forgot Password option to recover your account. You will be taken to a new page to recover your account after clicking.",
+         "To recover your account, you must complete 4 steps.",
+         "<strong>Step 1:</strong> confirm your account. You must first authenticate your account in this stage using either your email address or mobile number.",
+         "<strong>Step 2:</strong> Enter the code. A code will be provided to your mobile phone or email address after verification in order to validate and approve your request to regain your account.",
+         "<strong>Step 3:</strong> Modify your password. You will be required to enter your desired new password in this stage.",
+         "<strong>Step 4:</strong> Complete Step 4 to change your password. This is the last step, after which you can login knowing that you have successfully changed your password."
+      ]
    },
    {
-      question: "What are the key components of YOLOv5?",
-      answer: "The key components of YOLOv5 include a backbone network (such as CSPDarknet53), neck modules (like PANet), and detection heads (YOLOv3-style). These components work together to detect objects at different scales and resolutions."
+      id: 4,
+      question: "How to change my personal information?",
+      answer: [
+         "Go to the Account settings menu and click the Edit button in the top right corner to modify your personal information. Once you click, a form will open up where you may change your personal information before clicking save."
+      ]
    },
    {
-      question: "How is YOLOv5 trained?",
-      answer: "YOLOv5 is trained using a large labeled dataset, where bounding box annotations are provided for objects of interest. The model is trained using techniques like data augmentation, transfer learning, and optimization algorithms to improve its detection performance."
+      id: 5,
+      question: "Can I customize the View History under Video Playback according to my needs?",
+      answer: [
+         "Apparently, you can't customize the View History but you can change the view based on your desired preferences such as changing it to list or grid view. You can also rename and delete if needed."
+      ]
    },
    {
-      question: "Can YOLOv5 detect different object classes?",
-      answer: "Yes, YOLOv5 can detect various object classes. It is typically trained on datasets that contain multiple object classes, such as the popular COCO dataset, which includes common objects like people, cars, animals, and more."
+      id: 6,
+      question: "Where can I see the saved videos of the surveillance occurred in a day?",
+      answer: [
+         "Unfortunately, there is no saved videos of the surveillance occurred in a day. Instead, there will be saved images every detection occurred. This can be seen at the Video Playback menu under the View History."
+      ]
    },
    {
-      question: "What programming language is YOLOv5 implemented in?",
-      answer: "YOLOv5 is implemented using Python programming language and popular deep learning frameworks like PyTorch. It leverages the power of these frameworks to build, train, and deploy the object detection model."
+      id: 7,
+      question: "What types of data can be displayed in the dashboard?",
+      answer: [
+         "Dashboards can display a wide range of data, including list of users, video playback for monitoring the surveillance camera, account settings for personal information and password settings, and help for the list of frequently asked questions."
+      ]
    },
    {
-      question: "What are the applications of YOLOv5?",
-      answer: "YOLOv5 has various applications in computer vision and beyond. It can be used for object detection in autonomous driving, surveillance systems, robotics, image and video analysis, and many other fields that require accurate and efficient object detection."
+      id: 8,
+      question: "Is the dashboard accessible from different devices?",
+      answer: [
+         "Yes, it is accessible from different devices since it is a web-based system but it do not have a mobile version or mobile app for this."
+      ]
    },
    {
-      question: "Are pre-trained YOLOv5 models available?",
-      answer: "Yes, pre-trained YOLOv5 models are available, which can be used as a starting point for object detection tasks. These models are trained on large datasets and can be fine-tuned or used directly for specific applications."
+      id: 9,
+      question: "Is data in the dashboard secure?",
+      answer: [
+         "Data security is an essential aspect of dashboard systems. Most platforms employ security measures such as user authentication, role-based access control, and encryption to ensure that only authorized individuals can access and view the data on the dashboard."
+      ]
    },
    {
-      question: "How can I get started with YOLOv5?",
-      answer: "To get started with YOLOv5, you can refer to the official YOLOv5 GitHub repository, which provides documentation, example code, and pre-trained models. You can explore the code, train your own models, and deploy them in your applications."
+      id: 10,
+      question: "How is the data presented in the dashboard?",
+      answer: [
+         "Data on a dashboard is typically presented using visual elements such as tables, images, and list views. These visualizations make it easier for users to grasp complex information quickly."
+      ]
    }
-   // Add more FAQ items as needed
 ];
